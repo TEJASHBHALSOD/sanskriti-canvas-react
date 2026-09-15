95205d2d7df4b671513500068697c9649d52ea92
Tejash <tejashbhai550@gmail.com>
2026-09-15 19:29:52 +0530

    add home, about, contact in route

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 
 import './App.css'
-import { BrowserRouter, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,14 +17,9 @@ function App() {
 
       <Navbar/>
       <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About/>}/>
-          <Route exact path="/service" element={<Service />}/>
-          <Route exact path="/contact" element={<Contact/>} />
-          <Route exact path="/goldloan" element={<GoldLoans/>}/>
-          <Route exact path="/personalLoan" element={<PersonalLoans/>}/>
-          <Route exact path="/businessloan" element={<Businessloan/>}/>
-          <Route exact path="/homeloan" element={<Homeloan/>} />
+          <Route exact path="/" element={<Home />} ></Route>
+          <Route exact path="/about" element={<About/>} ></Route>
+          <Route exact path="/contact" element={<Contact/>} ></Route>
         </Routes>
       </Router>
     </div>
